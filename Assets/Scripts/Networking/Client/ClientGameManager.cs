30b1b04f37181763419c0e1c271f8ffc5ea82a5a
ilya-nuhi <92297205+ilya-nuhi@users.noreply.github.com>
2024-04-25 23:58:11 +0300

    closing network safely

--- Assets/Scripts/Networking/Client/ClientGameManager.cs
+++ Assets/Scripts/Networking/Client/ClientGameManager.cs
@@ -1,26 +1,29 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ClientGameManager
+public class ClientGameManager : IDisposable
 {
     private JoinAllocation allocation;
+    private NetworkClient networkClient;
 
     private const string MenuSceneName = "Menu";
 
     public async Task<bool> InitAsync()
     {
         await UnityServices.InitializeAsync();
-
+        networkClient = new NetworkClient(NetworkManager.Singleton);
         AuthState authState = await AuthenticationWrapper.DoAuth();
 
         if(authState == AuthState.Authenticated)
@@ -54,7 +57,22 @@ public class ClientGameManager
         RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
         transport.SetRelayServerData(relayServerData);
 
+        UserData userData = new UserData
+        {
+            userName = PlayerPrefs.GetString(NameSelector.PlayerNameKey, "Missing Name"),
+            userAuthId = AuthenticationService.Instance.PlayerId
+        };
+        string payload = JsonUtility.ToJson(userData);
+        byte[] payloadBytes = Encoding.UTF8.GetBytes(payload);
+
+        NetworkManager.Singleton.NetworkConfig.ConnectionData = payloadBytes;
+
+
         NetworkManager.Singleton.StartClient();
     }
 
+    public void Dispose()
+    {
+        networkClient?.Dispose();
+    }
 }
--- Assets/Scripts/Networking/Host/HostSingleton.cs
+++ Assets/Scripts/Networking/Host/HostSingleton.cs
@@ -36,4 +36,10 @@ public class HostSingleton : MonoBehaviour
     {
         GameManager = new HostGameManager();
     }
+
+    private void OnDestroy()
+    {
+        GameManager?.Dispose();
+    }
+
 }
